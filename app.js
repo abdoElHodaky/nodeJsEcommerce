1a09de5afe90ebc184293b977c3cddf21c151212
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2022-02-20 21:51:05 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var app = express();
 var http = require('http');
 var server = http.createServer(app);
-server.listen(process.env.PORT||3000)
+server.listen(process.env.PORT || "3000")
 var io=require('socket.io')(server)
 var index = require('./routes/controll')(io);
 // view engine setup

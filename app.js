ba1116b8b9f4ae507533897d5ac48c99408c59ab
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2022-02-21 10:42:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var app = express();
-app.listen(process.env.PORT || 3000,console.log)
+//app.listen(process.env.PORT || 3000,console.log)
 //var http = require('http');
 //var server = http.createServer(app);
 //server.listen(process.env.PORT || "3000")
@@ -57,5 +57,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+app.listen(process.env.PORT || '3000',console.log)
 module.exports = app;

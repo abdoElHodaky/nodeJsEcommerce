b0cfc3a5bd7903d7dbf49233a8a6e5fd28f58cb9
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2022-02-25 16:51:57 +0200

    Update knex.js

--- knex.js
+++ knex.js
@@ -1,10 +1,10 @@
 var knex = require('knex')({
   client: 'mysql',
   connection: {
-    host : 'sql.db4free.net',
-    user : 'abdo_ecommrce',
-    password : 'arh.27934',
-    database : 'ecommerce2'
+    host : process.env.DATABASE_HOST         || 'sql.db4free.net',
+    user : process.env.DTATBASE_USER         || 'abdo_ecommrce',
+    password : process.env.DATABASE_PASSWORD || 'arh.27934',
+    database : process.env.DATABASE_NAME     || 'ecommerce2'
   }
 });
 module.exports=knex

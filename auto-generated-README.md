969a0a1793383d948879518d86a773093efd6707
abdoElHodaky <abdo.arh38@yahoo.com>
2022-02-28 16:35:51 +0200

    Add pretty readme

---
+++ auto-generated-README.md
@@ -0,0 +1,39 @@
+# nnodeJsEcommerce
+
+  ## Description
+
+  simple ecommerce site with Node.js 
+
+  ## Table of Contents
+
+  - [Description](#description)
+  - [Install](#install)
+  - [Usage](#usage)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  - [Credits](#credits)
+  - [License](#license)
+
+  ## Installation
+
+  change database connect after run npm install then npm run start 
+
+  ## Usage
+
+  run npm run start 
+
+  ## Tests
+
+  
+
+  ## Questions
+
+  use my contact info 
+  
+  ## Credits
+
+  [@abdoElHodaky](https://github.com/abdoElHodaky)
+
+  ## License
+  
+  [![License: The Unlicense](https://img.shields.io/badge/license-The Unlicense-blue.svg)](http://The Unlicense.org/)
---
+++ node_modules/readme-generator-cli/utils/generateMarkdown.js
@@ -0,0 +1,55 @@
+// function to generate markdown for README
+function generateMarkdown({
+  title,
+  description,
+  install,
+  usage,
+  tests,
+  questions,
+  credits,
+  license,
+  color,
+}) {
+  return `# ${title}
+
+  ## Description
+
+  ${description}
+
+  ## Table of Contents
+
+  - [Description](#description)
+  - [Install](#install)
+  - [Usage](#usage)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  - [Credits](#credits)
+  - [License](#license)
+
+  ## Installation
+
+  ${install}
+
+  ## Usage
+
+  ${usage}
+
+  ## Tests
+
+  ${tests}
+
+  ## Questions
+
+  ${questions}
+  
+  ## Credits
+
+  [@${credits}](https://github.com/${credits})
+
+  ## License
+  
+  [![License: ${license}](https://img.shields.io/badge/license-${license}-${color}.svg)](http://${license}.org/)
+`;
+}
+
+module.exports = generateMarkdown;
